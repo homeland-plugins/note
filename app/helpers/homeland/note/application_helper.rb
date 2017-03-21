b0d1251bfe9034f7061e74fd18945902a3f5275b
Jason Lee <huacnlee@gmail.com>
2017-03-21 20:13:05 +0800

    Remove unused helper

--- app/helpers/homeland/note/application_helper.rb
+++ app/helpers/homeland/note/application_helper.rb
@@ -1,9 +1,5 @@
 module Homeland::Note
   module ApplicationHelper
-    def render_node_name(name, id)
-      link_to(name, main_app.node_topics_path(id), class: 'node')
-    end
-
     def note_title_tag(note, opts = {})
       opts[:limit] ||= 50
       return '' if note.blank?
