caf828d98c6648f205b077a733f44c0e7461998a
Jason Lee <huacnlee@gmail.com>
2017-03-21 19:43:21 +0800

    Version 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,4 @@
-0.1.1
------
-
-- Fix typo bug when `note` not enable.
-
-0.1.0
+0.1.2
 -----
 
 - First release.
--- lib/homeland/note/version.rb
+++ lib/homeland/note/version.rb
@@ -2,6 +2,6 @@ module Homeland
   module Note
     NAME    = 'note'
     DESCRIPTION = 'Note for Homeland.'
-    VERSION = '0.1.1'
+    VERSION = '0.1.2'
   end
 end
